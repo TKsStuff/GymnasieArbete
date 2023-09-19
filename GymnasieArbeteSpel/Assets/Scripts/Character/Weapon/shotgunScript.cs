b92f32e2091b5c49f2b6e6d8ebd5d3df0130915e
Takuan <davidwallgren@outlook.com>
2023-09-19 16:37:05 +0200

    added enemy and gun script not complete
    follow doom clone guide E1M2

--- GymnasieArbeteSpel/Assets/Scripts/Character/Weapon/shotgunScript.cs
+++ GymnasieArbeteSpel/Assets/Scripts/Character/Weapon/shotgunScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,22 +9,32 @@ public class shotgunScript : MonoBehaviour
 
     private bool isShooting;
     public Animation shotgunRecoil;
+    private BoxCollider gunTrigger;
+    public float range =  20f;
+    public float vRange = 20f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gunTrigger = GetComponent<BoxCollider>();
+        gunTrigger.size = new Vector3(1,vRange,range);
+        gunTrigger.center = new Vector3(0,0, range * 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shoot();
     }
 
     void shoot()
     {
+        if(Input.GetKey(KeyCode.Mouse0)) 
+        {
+            Debug.Log("shooting");
+        }
+
 
     }
 
