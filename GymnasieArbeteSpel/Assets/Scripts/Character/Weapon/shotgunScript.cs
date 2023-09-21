97983950af6cc58403e28b14559a09c13542aa67
Takuan <davidwallgren@outlook.com>
2023-09-21 23:06:00 +0200

    kinda working gun reg

--- GymnasieArbeteSpel/Assets/Scripts/Character/Weapon/shotgunScript.cs
+++ GymnasieArbeteSpel/Assets/Scripts/Character/Weapon/shotgunScript.cs
@@ -7,62 +7,48 @@ public class shotgunScript : MonoBehaviour
 {
 
 
-    private bool isShooting;
-    public Animation shotgunRecoil;
     private BoxCollider gunTrigger;
-    public float range =  20f;
-    public float vRange = 20f;
-
+    public float range;
+    public float vRange;
+    public EnemyManager enemyManager;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         gunTrigger = GetComponent<BoxCollider>();
-        gunTrigger.size = new Vector3(1,vRange,range);
-        gunTrigger.center = new Vector3(0,0, range * 0.5f);
+        gunTrigger.size = new Vector3(1, vRange, range);
+        gunTrigger.center = new Vector3(0, 0, range * 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.transform.GetComponent<Enemy>();
+        if (enemy) {
         
-    }
-
-    void shoot()
-    {
-        if(Input.GetKey(KeyCode.Mouse0)) 
-        {
-            Debug.Log("shooting");
-        }
-
-
-    }
-
-
-    void OnTriggerEnter(Collider other)
-    {
-        enemy Enemy = other.transform.GetComponent<enemy>();
-
-
-        if (Enemy)
-        {
-
+            enemyManager.AddEnemy(enemy);
+        
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        enemy Enemy = other.transform.GetComponent<enemy>();
-
-
-        if (Enemy)
+        Enemy enemy = other.transform.GetComponent<Enemy>();
+        if (enemy)
         {
 
+            enemyManager.RemoveEnemy(enemy);
         }
     }
 
-    void shotgunAnim()
-    {
 
-    }
+
+
+
 }
--- GymnasieArbeteSpel/Assets/Scripts/Enemy/enemyManager.cs
+++ GymnasieArbeteSpel/Assets/Scripts/Enemy/enemyManager.cs
@@ -1,26 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class enemyManager : MonoBehaviour
+public class EnemyManager : MonoBehaviour
 {
+    public List<Enemy> enemiesInTrigger = new List<Enemy>();
 
-    public List<enemy> enemiesInView = new List<enemy> ();
 
-    public void addEnemy(enemy Enemy)
+    public void AddEnemy(Enemy enemy)
     {
-
+        enemiesInTrigger.Add(enemy);
+    }
+    public void RemoveEnemy(Enemy enemy)
+    {
+        enemiesInTrigger.Remove(enemy);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
